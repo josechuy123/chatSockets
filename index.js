57d405beedf4c96d4b8b10de365ec638ee7c239b
josechuy123 <jdiaz33@ucol.mx>
2021-04-29 00:05:55 -0500

    comit base de datos

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 let app = require("express")();
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
+var mysql = require("mysql");
+
+var conn = mysql.createConnection({
+  host: "localhost",
+  database: "chat_progra",
+  user: "root",
+});
+
+conn.connect(function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Conexión exitosa!");
+  }
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/chat.html");
@@ -13,9 +28,18 @@ io.on("connection", function (socket) {
 
   socket.on("createUser", function (data) {
     username = data;
+    conn.query(
+      'INSERT INTO usuarios(nombre_usuario, fecha) VALUES ("' +
+        username +
+        '", CURDATE())'
+    );
   });
 
   socket.on("mjsNuevo", function (data) {
+    socket.broadcast.emit("mensaje", {
+      usuario: username,
+      mensaje: data,
+    });
     socket.emit("mensaje", {
       usuario: username,
       mensaje: data,
